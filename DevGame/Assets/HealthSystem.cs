0b7694e78eecad3abe758ff414b7063c931db07c
Luisotee <luisotaviomgr1@gmail.com>
2019-11-24 19:14:15 -0300

    Fixed boss health

---
+++ DevGame/Assets/HealthSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthSystem : MonoBehaviour
+ {
+    public Transform healthBar;
+    public Slider healthFill;
+
+    public float maxHealth;
+    public float healthBarYOffset = 1f;
+
+    public int health;
+
+    void Update()
+    {
+        if(transform.position.y < -40)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Destroy(GameObject.FindWithTag("PlayerHealthBar"));
+        }
+
+        PositionHealthBar();
+        ChangeHealth();
+        if (health <= 0)
+            {
+                Destroy(gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                Destroy (GameObject.FindWithTag("PlayerHealthBar"));
+            }
+    }
+ 
+    public void Damaged()
+    {
+        health -= 1;
+    }
+    
+    public void ChangeHealth()
+    {
+        healthFill.value = health / maxHealth;
+    }
+
+    private void PositionHealthBar()
+    {  
+        Vector3 currentPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        healthBar.position = new Vector3(currentPos.x, currentPos.y + healthBarYOffset, currentPos.z);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# TheJornay
-A game made for school purposes using Unity
+# The Jornay
+We are developing a game to a school project using unity.
+
+The game is a platform game with combat mechanics, it's still in a very early development stage, with only 1 playable map.
+You can see the unity file in the DevGame folder and can play it in the ExecGame.
