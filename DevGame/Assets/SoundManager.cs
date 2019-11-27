cb77802d8d90c86ef4a2f6cf7136ec4c1ce8f40e
Lucas de Abreu Furtado Garcia <52716819+Jornay@users.noreply.github.com>
2019-11-27 14:56:40 -0300

    MAPA FULL DO FULL

--- DevGame/Assets/SoundManager.cs
+++ DevGame/Assets/SoundManager.cs
@@ -10,7 +10,7 @@ public class SoundManager : MonoBehaviour
     public float lowPitchRange = .95f;
     public float highPitchRange = 1.05f;
 
-    void Awake()
+   /* void Awake()
     {
         if (instance == null)
         {
@@ -23,7 +23,7 @@ public class SoundManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
     }
-
+    */
     public void PlaySingle(AudioClip clip)
     {
         efxSource.clip = clip;
